8d83463efab51c1d44d56663bfdb98811e4786de
gpj283476091 <gpk283476091@gmail.com>
2021-03-19 15:55:40 +0530

    19-03-2021

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,30 @@
+package org.phone;
+
+public class PhoneInfo {
+public void phonename() {
+	System.out.println("redmi");
+}
+public void phonemeieium() {
+	System.out.println("8976");
+}
+public void phoneos() {
+	System.out.println("lolipop");
+
+}
+public void phonestorage() {
+	System.out.println("64");
+
+}
+public void phonecamera() {
+	System.out.println("32");
+
+}
+public static void main(String[] args) {
+	PhoneInfo p= new PhoneInfo();
+	p.phonecamera();
+	p.phonemeieium();
+	p.phonename();
+	p.phoneos();
+	p.phonestorage();
+}
+}
